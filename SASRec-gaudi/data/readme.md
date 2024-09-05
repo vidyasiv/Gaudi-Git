ba4dc5c52085fe645266971cdd148c91e66f3889
root <root@pggaudi-s005-nicl.nfra.io>
2024-09-05 06:39:37 +0000

    sasrec first

--- SASRec-gaudi/data/readme.md
+++ SASRec-gaudi/data/readme.md
@@ -1,3 +1,13 @@
 Get datasets (Amazon 2023)
 
+Amazon 2023 citation
+
+@article{hou2024bridging,
+  title={Bridging Language and Items for Retrieval and Recommendation},
+  author={Hou, Yupeng and Li, Jiacheng and He, Zhankui and Yan, An and Chen, Xiusi and McAuley, Julian},
+  journal={arXiv preprint arXiv:2403.03952},
+  year={2024}
+}
+
+
 wget https://datarepo.eng.ucsd.edu/mcauley_group/data/amazon_2023/raw/review_categories/All_Beauty.jsonl.gz
---
+++ SASRec-gaudi/data_preprocess.py
@@ -0,0 +1,72 @@
+import os
+import os.path
+import gzip
+import json
+from tqdm import tqdm
+from collections import defaultdict
+
+def parse(path):
+    g = gzip.open(path, 'rb')
+    for l in tqdm(g):
+        yield json.loads(l)
+        
+def preprocess(fname):
+    countU = defaultdict(lambda: 0)
+    countP = defaultdict(lambda: 0)
+    line = 0
+
+    file_path = f'./data/{fname}.jsonl.gz'
+    
+    # counting interactions for each user and item
+    for l in parse(file_path):
+        line += 1
+        asin = l['asin']
+        rev = l['user_id']
+        time = l['timestamp']
+        countU[rev] += 1
+        countP[asin] += 1
+    
+    usermap = dict()
+    usernum = 0
+    itemmap = dict()
+    itemnum = 0
+    User = dict()
+    
+    
+    for l in parse(file_path):
+        line += 1
+        asin = l['asin']
+        rev = l['user_id']
+        time = l['timestamp']
+        
+        threshold = 5
+            
+        if countU[rev] < threshold or countP[asin] < threshold:
+            continue
+        
+        if rev in usermap:
+            userid = usermap[rev]
+        else:
+            usernum += 1
+            userid = usernum
+            usermap[rev] = userid
+            User[userid] = []
+        
+        if asin in itemmap:
+            itemid = itemmap[asin]
+        else:
+            itemnum += 1
+            itemid = itemnum
+            itemmap[asin] = itemid
+        User[userid].append([time, itemid])
+    
+    for userid in User.keys():
+        User[userid].sort(key=lambda x: x[0])
+        
+    print(usernum, itemnum)
+    
+    f = open(f'./data/processed/{fname}.txt', 'w')
+    for user in User.keys():
+        for i in User[user]:
+            f.write('%d %d\n' % (user, i[1]))
+    f.close()
